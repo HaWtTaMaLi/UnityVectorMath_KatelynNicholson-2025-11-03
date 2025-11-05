6a999bbb9019c7937f555d7b6941f87d03336b5e
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-11-05 15:24:51 -0400

    push

--- Assets/Scripts/MoveTwoObjectsUsingSine.cs
+++ Assets/Scripts/MoveTwoObjectsUsingSine.cs
@@ -1,22 +1,31 @@
 using UnityEngine;
+using System;
 
 public class MoveTwoObjectsUsingSine : MonoBehaviour
 {
     //SCENE 1
-
-    public GameObject CubeOne;
-    public GameObject CubeTwo;
     //choose axis in inspector
     // use Sine 
+    private Vector3 startPos;
+
+    [SerializeField] 
+    private float frequency = 5f;
+
+    [SerializeField]
+    private float magnitude = 5f;
+
+    [SerializeField]
+    private float offset = 0f;
+
 
     void Start()
     {
-        
+        startPos = transform.position;
     }
 
     void Update()
     {
-        
+        transform.position = startPos + transform.up * Mathf.Sin(Time.time + frequency + offset) * magnitude;
     }
 
 }
